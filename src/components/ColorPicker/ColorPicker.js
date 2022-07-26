c1ae599dd3aafd95de1af5b3003b7ec7891b2846
AndreyHerzhov <andreyherzhov@gmail.com>
2022-07-26 16:08:44 +0300

    Update ColorPicker.js

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -2,18 +2,37 @@ import React, { Component } from "react";
 import './ColorPicker.css'
 
 class ColorPicker extends Component {
+    state = {
+        activeOptionIndex: 1 
+    }
+
+    makeOptionClassName = (index) => {
+        const optionClasses = ["ColorPicker__option"]
+
+        if (index === this.state.activeOptionIndex) {
+            optionClasses.push('ColorPicker__option--active')
+        }
+        return optionClasses.join(' ')
+    }
+    
     render() {
         return (
             <div className="ColorPicker">
                 <h2 className="ColorPicker__title">Color Pircker</h2>
                 <div>
-                    {this.props.options.map(({ label, color}) => (
-                        <button 
-                        key={label}
-                        className="ColorPicker__option"
-                        style={{backgroundColor: color}}
-                        ></button>
-                    ))}
+                    {this.props.options.map(({ label, color}, index) => {
+                    const optionClassName =  this.makeOptionClassName(index)    
+                        return (
+                            <button 
+                            key={label}
+                            className={optionClassName}
+                            style={{
+                                backgroundColor: color,
+
+                            }}
+                            ></button>
+                        )
+                    })}
                 </div>
             </div>
         )
